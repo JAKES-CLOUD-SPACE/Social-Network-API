e4052f487bf132a8eee3398399f8ff9fbc84fc3f
jake ringate <jakeringate3@gmail.com>
2025-02-25 11:15:19 -0600

    added thought routes

---
+++ routes/api/thoughts.js
@@ -0,0 +1,105 @@
+const express = require('express');
+const router = express.Router();
+const Thought = require('../../models/Thought');
+const User = require('../../models/User');
+
+// GET all thoughts
+router.get('/', async (req, res) => {
+  try {
+    const thoughts = await Thought.find();
+    res.json(thoughts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET a single thought by _id
+router.get('/:id', async (req, res) => {
+  try {
+    const thought = await Thought.findById(req.params.id);
+    if (!thought) {
+      return res.status(404).json({ message: 'Thought not found' });
+    }
+    res.json(thought);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// POST to create a new thought
+router.post('/', async (req, res) => {
+  try {
+    const { thoughtText, username, userId } = req.body;
+    const newThought = new Thought({ thoughtText, username, userId });
+
+    const user = await User.findById(userId);
+    user.thoughts.push(newThought);
+    await user.save();
+
+    await newThought.save();
+    res.json(newThought);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// PUT to update a thought by _id
+router.put('/:id', async (req, res) => {
+  try {
+    const thought = await Thought.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!thought) {
+      return res.status(404).json({ message: 'Thought not found' });
+    }
+    res.json(thought);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// DELETE a thought by _id
+router.delete('/:id', async (req, res) => {
+  try {
+    const thought = await Thought.findByIdAndDelete(req.params.id);
+    if (!thought) {
+      return res.status(404).json({ message: 'Thought not found' });
+    }
+    res.json({ message: 'Thought deleted' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// POST to create a reaction
+router.post('/:thoughtId/reactions', async (req, res) => {
+  try {
+    const thought = await Thought.findById(req.params.thoughtId);
+    if (!thought) {
+      return res.status(404).json({ message: 'Thought not found' });
+    }
+
+    thought.reactions.push(req.body);
+    await thought.save();
+    res.json(thought);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// DELETE to remove a reaction
+router.delete('/:thoughtId/reactions/:reactionId', async (req, res) => {
+  try {
+    const thought = await Thought.findById(req.params.thoughtId);
+    if (!thought) {
+      return res.status(404).json({ message: 'Thought not found' });
+    }
+
+    thought.reactions.pull({ _id: req.params.reactionId });
+    await thought.save();
+
+    res.json({ message: 'Reaction deleted' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
