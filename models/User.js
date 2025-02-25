619b799b1905246b5662d153e05453f50db7d1c5
jake ringate <jakeringate3@gmail.com>
2025-02-25 11:16:12 -0600

    added user schema

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create User Schema
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Please fill a valid email address']
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Thought'
+    }
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ]
+});
+
+// Virtual for friendCount
+userSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
+
+// Create User model
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
